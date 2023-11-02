8642135a7365e0e88c3605eb517f6f9eac6969ef
Vipin Kumar <vipinwap07@gmail.com>
2023-11-03 04:35:45 +0530

    Latest Update

--- WifiPasswordRevealer.py
+++ WifiPasswordRevealer.py
@@ -1,13 +1,13 @@
 import subprocess
 
-a = subprocess.check_output(['netsh', 'wlan', 'show', 'profile']).decode('utf-8').split('\n')
-a = [i.split(":")[1][1:-1] for i in a if "All User Profile" in i]
+wlanInfo = subprocess.check_output(['netsh', 'wlan', 'show', 'profile']).decode('utf-8').split('\n')
+wlanInfo = [i.split(":")[1][1:-1] for i in wlanInfo if "All User Profile" in i]
 
 print('╔══════════════════════════════════════╗')
 print(' ═════════════ vipinwap07 ═════════════')
 print('╚══════════════════════════════════════╝', end='\n\n')
 
-for i in a:
+for i in wlanInfo:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
     results = [b.split(':')[1][1:-1] for b in results if 'Key Content' in b]
     try:
@@ -16,11 +16,14 @@ for i in a:
     except IndexError:
         print("{:<20} | {:<}".format(i, ""))
         print('-'*40)
+
 print()
 print()
+
 print('_/﹋\_')
 print('(҂`_´)')
 print('<,︻╦╤─ ҉ - -')
 print('_/﹋\_', end='\n\n')
+
 print('Press Enter to Exit...', end=" ")
 input()
